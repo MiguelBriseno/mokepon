76c81ce73e0160d9f837e287bf59e5def3984c19
Miguel Briseño <miguel.briseno.bustos@gmail.com>
2023-03-29 18:06:41 -0600

    Listo, agregamos los cambios

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict'
+
+
+
+let btn_mokepon = document.getElementById('boton_mokepon')
+btn_mokepon.addEventListener("click", seleccionarMokepon)
+
+function seleccionarMokepon(){
+    alert("Elegiste un mokepon")
+}
